0e44f7d80fd8b76866147018f61f9ae413d212f4
CollectBrains <ruzanow.dmitry@gmail.com>
2018-07-26 20:47:22 +0300

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -6,6 +6,51 @@ $(function() {
 		return false;
 	});
 
+	setInterval(logClockTime, 1000);
+
+	function logClockTime(){
+		var time = getClockTime();
+		console.clear();
+		console.log(time);
+	}
+
+	function getClockTime(){
+		var date = new Date();
+		var time = "";
+
+		var time = {
+			hours: date.getHours(),
+			minutes: date.getMinutes(),
+			seconds: date.getSeconds(),
+			ampm: "AM"
+		};
+
+		if (time.hours === 12){
+			time.ampm = "PM";
+		} else if (time.hours > 12){
+			time.ampm = "PM";
+			time.hours -= 12;	
+		}
+
+		if (time.hours < 10){
+			time.hours = "0" + time.hours;
+		}
+
+		if (time.seconds < 10){
+			time.seconds = "0" + time.seconds;
+		}
+
+		if (time.minutes < 10){
+			time.minutes = "0" + time.minutes;
+		}
+
+		return time.hours + ":" +
+				time.minutes + ":" +
+				time.seconds + " " +
+				time.ampm;
+
+	}
+
 	svg4everybody();
 
 });
